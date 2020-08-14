bb697e52d469c96997964ce0cc38c3f9c795077a
crowz <karas639@gmail.com>
2020-08-14 16:45:37 +0900

    study

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -263,7 +263,7 @@
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-c.NotebookApp.notebook_dir = 'E:\dwkim_doc'
+c.NotebookApp.notebook_dir = 'C:\Users\uk0305\Desktop\python_study-master'
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
